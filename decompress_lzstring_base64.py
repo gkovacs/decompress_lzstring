562d6a2a85dd561f067f71a45700a2e2fc248080
Geza Kovacs <gkovacs@stanford.edu>
2016-03-26 20:58:05 -0700

    removed unnecessary function call

--- decompress_lzstring_base64.py
+++ decompress_lzstring_base64.py
@@ -1,5 +1,7 @@
+import array
+
 keyStrBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-keyStrUriSafe = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$"
+#keyStrUriSafe = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$"
 baseReverseDic = {'+': 62, '/': 63, '1': 53, '0': 52, '3': 55, '2': 54, '5': 57, '4': 56, '7': 59, '6': 58, '9': 61, '8': 60, '=': 64, 'A': 0, 'C': 2, 'B': 1, 'E': 4, 'D': 3, 'G': 6, 'F': 5, 'I': 8, 'H': 7, 'K': 10, 'J': 9, 'M': 12, 'L': 11, 'O': 14, 'N': 13, 'Q': 16, 'P': 15, 'S': 18, 'R': 17, 'U': 20, 'T': 19, 'W': 22, 'V': 21, 'Y': 24, 'X': 23, 'Z': 25, 'a': 26, 'c': 28, 'b': 27, 'e': 30, 'd': 29, 'g': 32, 'f': 31, 'i': 34, 'h': 33, 'k': 36, 'j': 35, 'm': 38, 'l': 37, 'o': 40, 'n': 39, 'q': 42, 'p': 41, 's': 44, 'r': 43, 'u': 46, 't': 45, 'w': 48, 'v': 47, 'y': 50, 'x': 49, 'z': 51}
 
 #import sys
@@ -17,17 +19,8 @@ def decompressFromBase64(input):
     return ''
   return lzdecompress(len(input), 32, input)
 
-def one_if_greater_than_zero(val):
-  if val > 0:
-    return 1
-  return 0
-
-def assign_array(arr, idx, val):
-  while len(arr) < idx+1:
-    arr.append(0)
-  arr[idx] = val
-
 def lzdecompress(length, resetValue, input):
+  #dictionary = []
   dictionary = []
   next = 0
   enlargeIn = 4
@@ -46,18 +39,22 @@ def lzdecompress(length, resetValue, input):
   data_position = resetValue
   data_index = 1
   for i in range(3):
-    assign_array(dictionary, i, i)
+    dictionary.append(i)
   bits = 0
   maxpower = pow(2, 2)
   power = 1
   while power != maxpower:
     resb = data_val & data_position
+    if resb > 0:
+      resb = 1
+    else:
+      resb = 0
     data_position >>= 1
     if data_position == 0:
       data_position = resetValue
       data_val = baseReverseDic[input[data_index]]
       data_index += 1
-    bits |= one_if_greater_than_zero(resb) * power
+    bits |= resb * power
     power <<= 1
   
   next = bits
@@ -67,12 +64,16 @@ def lzdecompress(length, resetValue, input):
     power = 1
     while power != maxpower:
       resb = data_val & data_position
+      if resb > 0:
+        resb = 1
+      else:
+        resb = 0
       data_position >>= 1
       if data_position == 0:
         data_position = resetValue
         data_val = baseReverseDic[input[data_index]]
         data_index += 1
-      bits |= one_if_greater_than_zero(resb) * power
+      bits |= resb * power
       power <<= 1
     c = unichr(bits)
   elif next == 1:
@@ -81,18 +82,22 @@ def lzdecompress(length, resetValue, input):
     power = 1
     while power != maxpower:
       resb = data_val & data_position
+      if resb > 0:
+        resb = 1
+      else:
+        resb = 0
       data_position >>= 1
       if data_position == 0:
         data_position = resetValue
         data_val = baseReverseDic[input[data_index]]
         data_index += 1
-      bits |= one_if_greater_than_zero(resb) * power
+      bits |= resb * power
       power <<= 1
     c = unichr(bits)
   elif next == 2:
     return ''
   
-  assign_array(dictionary, 3, c)
+  dictionary.append(c)
   w = c
   result.append(c)
   while True:
@@ -103,12 +108,16 @@ def lzdecompress(length, resetValue, input):
     power = 1
     while power != maxpower:
       resb = data_val & data_position
+      if resb > 0:
+        resb = 1
+      else:
+        resb = 0
       data_position >>= 1
       if data_position == 0:
         data_position = resetValue
         data_val = baseReverseDic[input[data_index]]
         data_index += 1
-      bits |= one_if_greater_than_zero(resb) * power
+      bits |= resb * power
       power <<= 1
     
     c = bits
@@ -119,14 +128,18 @@ def lzdecompress(length, resetValue, input):
       power = 1
       while power != maxpower:
         resb = data_val & data_position
+        if resb > 0:
+          resb = 1
+        else:
+          resb = 0
         data_position >>= 1
         if data_position == 0:
           data_position = resetValue
           data_val = baseReverseDic[input[data_index]]
           data_index += 1
-        bits |= one_if_greater_than_zero(resb) * power
+        bits |= resb * power
         power <<= 1
-      assign_array(dictionary, dictSize, unichr(bits))
+      dictionary.append(unichr(bits))
       dictSize += 1
       c = dictSize - 1
       enlargeIn -= 1
@@ -136,14 +149,18 @@ def lzdecompress(length, resetValue, input):
       power = 1
       while power != maxpower:
         resb = data_val & data_position
+        if resb > 0:
+          resb = 1
+        else:
+          resb = 0
         data_position >>= 1
         if data_position == 0:
           data_position = resetValue
           data_val = baseReverseDic[input[data_index]]
           data_index += 1
-        bits |= one_if_greater_than_zero(resb) * power
+        bits |= resb * power
         power <<= 1
-      assign_array(dictionary, dictSize, unichr(bits))
+      dictionary.append(unichr(bits))
       dictSize += 1
       c = dictSize - 1
       enlargeIn -= 1
@@ -163,7 +180,7 @@ def lzdecompress(length, resetValue, input):
         return ''
     result.append(entry)
     
-    assign_array(dictionary, dictSize, w + entry[0])
+    dictionary.append(w + entry[0])
     dictSize += 1
     enlargeIn -= 1
     
